b34f1b8b92569adb0cbeda6f4b2e59ace2cf2a70
Juan Diego Arismendi <arismendijuandiego70@gmail.com>
2025-05-09 03:09:50 -0400

    fix: set basename for BrowserRouter to support GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/pasantias-yuppie">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
